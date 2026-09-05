ef4ac7458828ca4f9fd26d5789747788ce601141
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-05 21:22:30 +0530

    feat: implement database connection and configuration setup

--- auth-backup/server.js
+++ auth-backup/server.js
@@ -1,5 +1,7 @@
 import app from './src/app/app.js'
+import { connectDB } from './src/config/db.js'
 
+await connectDB()
 app.listen(3000,()=>{
     console.log("Server is running")
 })
--- auth-backup/src/config/db.js
+++ auth-backup/src/config/db.js
@@ -1,3 +1,11 @@
 import mongoose from "mongoose";
+import config from "./config.js";
 
-export const connectDB = async () => {};
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(config.MONGO_URI)
+        console.log("Database is connected")
+    } catch (error) {
+        console.log("Error in connecting database",error)
+    }
+};
