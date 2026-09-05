577e3aa3d88ca8797208225d0f5cb3b754de4f37
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-05 22:35:42 +0530

    fix: correct typo in request body and update token generation in registerUserController

--- auth-backup/src/controllers/user.controller.js
+++ auth-backup/src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
 export const registerUserController = async (req, res) => {
-  const { name, email, password } = req.boady;
+  const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({
@@ -28,7 +28,7 @@ export const registerUserController = async (req, res) => {
     password: hashedPass,
   });
 
-  const token = jwt.sign({ user: _id }, config.JWT_SECRET);
+  const token = jwt.sign({ user: user._id }, config.JWT_SECRET);
 
   return res.status(201).json({
     message: "User is register",
@@ -38,5 +38,48 @@ export const registerUserController = async (req, res) => {
       email: user.email,
     },
     token,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All the login field is required",
+    });
+  }
+
+  const isExist = await userModel.findOne({ email });
+
+  if (!isExist) {
+    return res.status(400).json({
+      message: "User is not register",
+    });
+  }
+
+  const comparePass = await bcrypt.compare(password, user.password);
+
+  if (!comparePass) {
+    return res.status(400).json({
+      message: "Wrong pass",
+    });
+  }
+
+  const hashPass = await bcrypt.hash(password, 10);
+
+  const token = jwt.sign({ user: user._id }, config.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return res.status(200).json({
+    message: "Login successful",
+    data: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    token,
+    hashPass,
   });
 };
