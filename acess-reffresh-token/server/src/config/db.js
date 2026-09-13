94f4d82d116d4db54238db4fe11dea8526bb52e3
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-13 22:04:23 +0530

    feat: implement MongoDB connection with error handling

--- acess-reffresh-token/server/src/config/db.js
+++ acess-reffresh-token/server/src/config/db.js
@@ -1,5 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import config from "./config";
 
-export const connectDB =()=>{
-    mongoose.connect()
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGO_URI);
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log(`Error in connecting DB ${error}`);
+  }
+};
