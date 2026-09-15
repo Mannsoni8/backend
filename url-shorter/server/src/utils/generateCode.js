5f5a84f64ebbd907a529912e466aab9e66046a7f
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-15 22:34:47 +0530

    feat: implement generateCode function to create unique 6 character codes

---
+++ url-shorter/server/src/utils/generateCode.js
@@ -0,0 +1,17 @@
+import crypto from "crypto";
+
+/**
+ * Generate a 6 character code to generate the unique code
+ */
+
+const generateCode = () => {
+  const mainString =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  let shortCode = " ";
+  for (let i = 0; i < 6; i++) {
+    shortCode += mainString.charAt(Math.floor(Math.random() * 62));
+  }
+  return shortCode;
+};
+export default generateCode;
