a41821db04853a31acc11e0ec4c7ae0b2e067d84
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-15 23:34:48 +0530

    feat: implement codeUrlController for redirecting based on shortcode and improve URL validation logic

--- url-shorter/server/src/app/app.js
+++ url-shorter/server/src/app/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import router from '../routes/url.routes.js'
+import { codeUrlController } from '../controllers/url.controller.js'
 
 const app = express()
 app.use(express.json())
@@ -9,5 +10,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/url',router)
+
+app.get("/:code",codeUrlController)
 
 export default app
--- url-shorter/server/src/controllers/url.controller.js
+++ url-shorter/server/src/controllers/url.controller.js
@@ -2,64 +2,70 @@ import urlModel from "../model/url.model.js";
 import generateCode from "../utils/generateCode.js";
 
 export const getUrlShortcodeController = async (req, res) => {
-  const { url } = req.body;
+    const { url } = req.body;
 
-  if (!url) {
-    return res.status(400).json({
-      error: "Please enter a URL",
+    if (!url) {
+        return res.status(400).json({
+            error: "Please enter a URL",
+        });
+    }
+
+    if (
+        !url.startsWith("http://") &&
+        !url.startsWith("https://")
+    ) {
+        return res.status(400).json({
+            error: "Please enter a valid URL starting with http:// or https://",
+        });
+    }
+
+    if (url.length > 2048) {
+        return res.status(400).json({
+            error: "URL is too long",
+        });
+    }
+
+    const code = generateCode();
+
+    const newUrl = await urlModel.create({
+        originalUrl: url,
+        shortCode: code,
     });
-  }
 
-  if (
-    !url.startsWith("http://") == false &&
-    !url.startsWith("https://") == false
-  ) {
-    return res.status(400).json({
-      error: "Please enter a valid URL start with http:// or https://",
+    return res.status(201).json({
+        message: "URL shortened successfully",
+        data: {
+            originalUrl: newUrl.originalUrl,
+            shortCode: newUrl.shortCode,
+        },
     });
-  }
-
-  if (url.length > 2048) {
-    return res.status(400).json({ error: "URL is to long" });
-  }
-
-  const code = generateCode();
-
-  const newUrl = await urlModel.create({
-    originalUrl: url,
-    shortCode: code,
-  });
-
-  return res.status(201).json({
-    message: "URL shortened successfully",
-    data: {
-      originalUrl: newUrl.originalUrl,
-      shortCode: newUrl.shortCode,
-    },
-  });
 };
+
 
 export const getUrl = async (req, res) => {
-  const urls = await urlModel.find();
+    const urls = await urlModel.find();
 
-  return res.status(200).json({
-    message: "URL fetched successfully",
-    data: {
-      urls,
-    },
-  });
+    return res.status(200).json({
+        message: "URL fetched successfully",
+        data: {
+            urls,
+        },
+    });
 };
 
-export const codeUrlController = async(req,res)=>{
-    const {code}= req.params
+
+export const codeUrlController = async (req, res) => {
+    const { code } = req.params;
 
     const url = await urlModel.findOne({
-        shortCode:code
-    })
-   if (!url) {
-    return res.status(400).json({
-      error: "Please enter a URL",
+        shortCode: code,
     });
-  }
-  res.redirect(302,url.originalUrl)
-}
+
+    if (!url) {
+        return res.status(404).json({
+            error: "Short URL not found",
+        });
+    }
+
+    return res.redirect(302, url.originalUrl);
+};
