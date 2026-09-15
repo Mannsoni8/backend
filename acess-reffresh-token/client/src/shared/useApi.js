26de4476811f848a7556c4baecc364d1f7a37411
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-15 12:25:03 +0530

    feat: implement useApi hook with axios for API requests and authorization handling

--- acess-reffresh-token/client/src/shared/useApi.js
+++ acess-reffresh-token/client/src/shared/useApi.js
@@ -1,1 +1,19 @@
-import axios from "axios"
+import axios from "axios";
+import { useAuthContext } from "../modules/auth/context/useAuthContext";
+
+export default function useApi() {
+  const authContext = useAuthContext();
+
+  const api = axios.create({
+    baseURL: "http://localhost:5173/api",
+    withCredentials: true, //cookies ko set ya read kr paye
+  });
+
+  api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${authContext.accessToken}`;
+
+    return config;
+  });
+
+  return api;
+}
