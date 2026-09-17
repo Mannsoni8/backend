844ac30e8ad061bf5701e8b710ed46348170a251
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-17 18:23:52 +0530

    feat: implement URL fetching and shortening functionality with axios integration

--- url-shorter/client/src/app/App.jsx
+++ url-shorter/client/src/app/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 const dumyUrls = [
   {
@@ -23,28 +24,83 @@ const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [currentUrl, setCurrentUrl] = useState(null);
 
+  async function fetchUrls() {
+    const response = await axios.get("http://localhost:5173/api/url");
+
+    const responseData = response.data;
+
+    setUrls(responseData.data.urls);
+
+    console.log(responseData);
+  }
+
+  async function createShortUrl() {
+    const res = await axios.post("http://localhost:5173/api/url", {
+      url: inputValue,
+    });
+
+    setCurrentUrl({
+      orginalUrl: res.data.data.orginalUrl,
+      shortCode: res.data.data.shortCode,
+    });
+    fetchUrls();
+  }
+
+  async function deleteUrl() {
+    await axios.delete(`http://localhost:5173/api/url/${id}`);
+
+    fetchUrls();
+  }
+
+  useEffect(() => {
+    fetchUrls();
+  }, []);
+
   return (
-    <section className="p-10 flex flex-col gap-4">
-      <div className="w-full max-w-4xl p-2"></div>
-      <div className="w-full max-w-4xl p-2"></div>
-      <div className="w-full max-w-4xl p-2">
+    <main className="p-10 flex flex-col gap-4">
+      <div className="w-full max-w-4xl p-2 flex gap-2">
+        <input
+          type="text"
+          placeholder="Enter Long URL"
+          value={inputValue}
+          className="border rounded w-full p-2"
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+        <button
+          className="rounded p-2 bg-orange-600 text-white cursor-pointer"
+          onClick={createShortUrl}>
+          Shorten
+        </button>
+      </div>
+      <div className="w-full max-w-4xl p-2 "></div>
+      <div className="w-full max-w-4xl p-2 flex flex-col gap-2">
         {urls.map((url) => {
           return (
-            <div
-              key={url._id}
-              className="border border-neutral-200 p-2 flex flex-col gap-8 justify-evenly">
-              <p className="truncate">{url.orginalUrl}</p>
-              <div className="flext gap-2">
-                <button className="p-2 rounded bg-orange-600 text-white cursor-pointer">COPY</button>
-                <button className="p-2 rounded bg-orange-600 text-white cursor-pointer">DELETE</button>
+            <div className="border border-neutral-200 p-2 flex gap-8 justify-evenly items-center">
+              <a
+                href={`http://localhost:3000/${url.shortCode}`}
+                target="_blank">
+                {url.shortCode}
+              </a>
+              <p className="truncate">{url.originalUrl}</p>
+              <p>{url.clicks}</p>
+              <div className="flex gap-2">
+                <button className="p-2 rounded bg-orange-600 text-white cursor-pointer">
+                  COPY
+                </button>
+                <button
+                  onClick={() => deleteUrl(url._id)}
+                  className="p-2 rounded bg-orange-600 text-white cursor-pointer">
+                  DELETE
+                </button>
               </div>
-              <p>Short Code: {url.shortCode}</p>
-              <p>Clicks: {url.clicks}</p>
             </div>
           );
         })}
       </div>
-    </section>
+    </main>
   );
 };
 
