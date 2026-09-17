89746d89fb29bdd1885d0046a7ccf0729f3a32e3
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-17 16:32:40 +0530

    refactor: improve formatting of App component and update return structure

--- url-shorter/client/src/app/App.jsx
+++ url-shorter/client/src/app/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 
 const App = () => {
   const [urls, setUrls] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [currentUrl, setCurrentUrl] = useState(null);
 
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 };
 
 export default App;
