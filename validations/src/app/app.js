d22420e3279bf5a016b1c9a2c725836cc9c7a9b3
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-20 16:33:43 +0530

    feat: set up authentication routes in Express app

--- validations/src/app/app.js
+++ validations/src/app/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import route from "../routes/auth.routes.js";
 
-const app = express()
-app.use(express.json())
+const app = express();
 
-export default app
+app.use(express.json());
+app.use("/api/auth", route);
+
+export default app;
