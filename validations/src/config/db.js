59f3077b25730ad173a644734719413e65f95166
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-20 16:29:52 +0530

    feat: establish database connection and set up authentication routes

--- validations/src/config/db.js
+++ validations/src/config/db.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose"
+import config from "./config.js"
+
+async function ConnecteDB() {
+    try {
+        await mongoose.connect(config.MONGO_URI)
+        console.log("Server is connected to DB")
+    } catch (error) {
+        console.log("Error in connecting DB",error)
+    }
+}
+
+export default ConnecteDB
--- validations/src/server.js
+++ validations/src/server.js
@@ -1,5 +1,7 @@
 import app from "./app/app.js"
+import ConnecteDB from "./config/db.js"
 
+await ConnecteDB()
 app.listen(3000,()=>{
     console.log("Server is running")
 })
