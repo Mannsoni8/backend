64fcad94f4771a1a7d058799bb7dc562a0313e65
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-22 20:38:30 +0530

    fix: correct request body format in product route documentation

--- snitch/server/src/router/product.route.js
+++ snitch/server/src/router/product.route.js
@@ -6,7 +6,7 @@ const productRouter = Router()
  * @method POST
  * @route /qpi/products/
  * @description creates the product and save its data into the DB,images will be store in imageKit.
- * req.body=>{title,description:price:{amount,currency},size:[()]
+ * req.body=>{title,description:price:{amount,currency},size:[{size,stock}]
  */
 
 
