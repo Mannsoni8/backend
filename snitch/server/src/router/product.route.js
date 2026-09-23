f7988560f1dd69549bfa1d74c79c7c129f873d29
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-23 21:26:44 +0530

    feat: add product creation route with authentication and image upload handling

--- snitch/server/src/router/product.route.js
+++ snitch/server/src/router/product.route.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
+import { authenticate } from "../middleware/auth.middleware.js";
+import { createProduct } from "../controller/product.controller.js";
+import multer from "multer";
 
-const productRouter = Router()
+const upload = multer({ storage: multer.memoryStorage() });
+
+const productRouter = Router();
 
 /**
  * @method POST
@@ -9,5 +14,24 @@ const productRouter = Router()
  * req.body=>{title,description:price:{amount,currency},size:[{size,stock}]
  */
 
+Router.post(
+  "/",
+  authenticate,
+  (req, res, next) => {
+    if (req.user.role != "seller") {
+      return res.status(403).json({
+        message: "user is not authorize to create products",
+      });
+    }
+    next();
+  },
+  upload.array("images"),
+  (req, res, next) => {
+    req.body.price = JSON.parse(req.body.price);
+    req.body.size = JSON.parse(req.body.size);
+    next();
+  },
+  createProduct,
+);
 
-export default productRouter
+export default productRouter;
