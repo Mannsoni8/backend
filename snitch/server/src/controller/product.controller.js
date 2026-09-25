22943cd920dc664efc41d9b881458a40cd4b2417
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-25 15:53:38 +0530

    refactor: improve formatting and consistency in createProduct and listAllProducts functions

--- snitch/server/src/controller/product.controller.js
+++ snitch/server/src/controller/product.controller.js
@@ -9,20 +9,24 @@ export async function createProduct(req, res) {
     message: "Dummy response",
   });
 
-  const fileUrls = []
-  for(let i=0;i<req.files.length;i++){
+  const fileUrls = [];
+  for (let i = 0; i < req.files.length; i++) {
     const responce = await uploadFile({
-      buffer:req.files[ i ].buffer,
-      filename:req.files[ i ].originalname
-    })
-    console.log(responce)
+      buffer: req.files[i].buffer,
+      filename: req.files[i].originalname,
+    });
+    console.log(responce);
   }
 }
 // non-handel  attack - ddos ,
 
-export async function listAllProducts(req,res) {
-  
-  const products = await productModel.find()
+export async function listAllProducts(req, res) {
+  const products = await productModel.find();
 
-  res.status(200)
+  res.status(200).json({
+    message: "Products data fetched successfully",
+    data: {
+      products,
+    },
+  });
 }
